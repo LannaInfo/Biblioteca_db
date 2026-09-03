04a95437956ea7d576c5320079b9551a0cc9ab16
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-03 14:28:06 -0300

    usuarios

---
+++ dados_user.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("Biblioteca.db")
+
+conn.execute('DROP TABLE  IF EXISTS usuarios')
+
+#criar a tabela usuarios
+conn.execute('CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome  TEXT NOT NULL)')
+
+conn.executemany("INSERT INTO usuarios(nome) VALUES(?)",
+                 [("Carlos",), ("Mateus",), ("Ana Laura",),])
+
+
+conn.commit()
+
--- inserir_dados.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("Biblioteca.db")
-conn.executemany("INSERT INTO usuarios(nome) VALUES(?)",
-                 [("Carlos",), ("Mateus",), ("Ana Laura",),])
-conn.commit()
